dcd233a3e420a1aad528edfebd6f50ebfa640e7e
Javier <pera.vidal.javier@gmail.com>
2025-10-15 20:42:49 +0200

    15

---
+++ Ejercicio6/src/main/java/com/Ejercicio6/repository/ClienteRepository.java
@@ -0,0 +1,20 @@
+package com.Ejercicio6.repository;
+
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Ejercicio6.modelo.Cliente;
+
+
+
+public interface ClienteRepository extends JpaRepository<Cliente,Integer>{
+
+	List<Cliente> findByNombreStartingWithIgnoreCase(String letra);
+
+	List<Cliente> findByClientesByCiudad(String ciudad);
+	
+}
---
+++ Ejercicio6/src/main/java/com/Ejercicio6/servicio/ClienteServiceImp.java
@@ -0,0 +1,88 @@
+package com.Ejercicio6.servicio;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.Ejercicio6.modelo.Cliente;
+import com.Ejercicio6.modelo.Direccion;
+import com.Ejercicio6.repository.ClienteRepository;
+import com.Ejercicio6.repository.DireccionRepository;
+
+public class ClienteServiceImp implements ClienteServicio{
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	private DireccionRepository direccionRepository;
+
+	@Override
+	public List<Cliente> getAllClientes() {
+		return clienteRepository.findAll();
+	}
+
+	@Override
+	public Cliente getClienteById(Integer id) {
+		return clienteRepository.findById(id).orElse(null);		
+	}
+
+	@Override
+	public void saveCliente(Cliente cliente) {
+	//Sin usar cascade
+	Direccion direccion = cliente.getDireccion();
+	if(direccion != null) {
+		direccionRepository.save(direccion); //Sin usar cascade
+	}
+		clienteRepository.save(cliente);
+	}
+
+	@Override
+	public Cliente updateCliente(Integer idCliente, Cliente cliente) {
+		Cliente optCliente = getClienteById(idCliente);
+		if(optCliente == null) {
+			return null;
+		}
+		optCliente.setNombre(cliente.getNombre());;
+		Direccion nuevaDireccion = cliente.getDireccion();
+			if(nuevaDireccion != null) {
+				nuevaDireccion.setId(cliente.getDireccion().getId());
+				direccionRepository.save(nuevaDireccion);
+				cliente.setDireccion(nuevaDireccion);
+			}
+			return clienteRepository.save(optCliente);
+	}
+
+	@Override
+	public Boolean deleteCliente(Integer id) {
+		Cliente cliente = getClienteById(id);
+		if(cliente != null) {
+			clienteRepository.deleteById(id);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public Cliente actualizarDireccion(Integer id, Direccion direccion) {
+		Cliente optCliente = getClienteById(id);
+		if(optCliente == null) {
+			return null;
+		}
+		direccion.setId(optCliente.getDireccion().getId());
+		direccionRepository.save(direccion);
+		optCliente.setDireccion(direccion);
+		return clienteRepository.save(optCliente);
+	}
+
+	@Override
+	public void findByNombreStartingWithIgnoreCase(String letra) {	
+		 List<Cliente> c = clienteRepository.findByNombreStartingWithIgnoreCase(letra);
+		for (Cliente cliente : c) {
+			Direccion d = cliente.getDireccion();
+			d.setCiudad("Sevilla");
+			direccionRepository.save(d);
+		}
+	}
+	
+	@Override
+	public List<Cliente> findByClientesByCiudad(String ciudad){
+		return clienteRepository.findByClientesByCiudad(ciudad);
+	}
+}
