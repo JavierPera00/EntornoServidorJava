beef05e9cc05a0361b5f159aecb788e799c2c959
JavierPera00 <pera.vidal.javier@gmail.com>
2025-10-29 18:17:27 +0100

    x

--- Ejercicio10/src/main/java/com/Ejercicio10/repository/OficinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Ejercicio10.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Ejercicio10.modelo.Oficina;
-
-public interface OficinaRepository extends JpaRepository<Oficina, Integer> {
-
-}
