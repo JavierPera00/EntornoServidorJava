b14f937e151e1f54938f6a15d3eed0ca201b2965
Javier <pera.vidal.javier@gmail.com>
2025-12-03 20:48:15 +0100

    x

--- EjercicioRepaso/src/main/java/com/EjercicioRepaso/EjercicioRepasoApplication.java
+++ EjercicioRepaso/src/main/java/com/EjercicioRepaso/EjercicioRepasoApplication.java
@@ -1,11 +1,19 @@
 package com.EjercicioRepaso;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.EjercicioRepaso.service.RecetaService;
 
 @SpringBootApplication
 public class EjercicioRepasoApplication {
 
+	@Autowired
+	RecetaService recetaService;
+	@Autowired
+	MuestrarioService MuestrarioService;
+	@Autowired
+	IngredienteService IngredienteService;
 	public static void main(String[] args) {
 		SpringApplication.run(EjercicioRepasoApplication.class, args);
 	}
